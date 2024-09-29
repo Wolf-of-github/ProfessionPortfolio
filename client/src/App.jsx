defe122710a578da22be13b00b3bd0743b0bf055
Wolf-of-github <iapte@usc.edu>
2024-09-28 23:42:09 -0700

    header deleted

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
